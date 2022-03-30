b5562641dce360f9ee2445a0b7847075cda1791f
Konakalla <895207@cognizant.com>
2022-03-30 14:51:18 +0530

    sonnar

--- UserService/src/main/java/com/flightapp/exception/GlobalExceptionHandler.java
+++ UserService/src/main/java/com/flightapp/exception/GlobalExceptionHandler.java
@@ -25,7 +25,7 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public Map<String, String> handleValidationExceptions(
 	  MethodArgumentNotValidException ex) {
-	    Map<String, String> errors = new HashMap<>();
+		Map<String, String> errors = new HashMap<>();
 	    ex.getBindingResult().getAllErrors().forEach((error) -> {
 	        String fieldName = ((FieldError) error).getField();
 	        String errorMessage = error.getDefaultMessage();
